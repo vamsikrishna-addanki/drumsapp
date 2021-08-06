76ff017ce3ef2273d62dc55fa0935728006860a4
vamsikrishna addanki <73530060+vamsikrishna-addanki@users.noreply.github.com>
2021-08-06 21:14:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,32 +12,32 @@ document.addEventListener("keypress", function(event){
 
 function makeSound(key){
 	switch(key){
-		case "q": var tom1 = new Audio("sounds/1.mp3");
+		case "a": var tom1 = new Audio("sounds/1.mp3");
 		          tom1.play();
 		          break;
-		case "w": var tom2 = new Audio("sounds/2.mp3");
+		case "s": var tom2 = new Audio("sounds/2.mp3");
 		          tom2.play();
 		          break;
-		case "e": var tom3 = new Audio("sounds/3.mp3");
+		case "d": var tom3 = new Audio("sounds/3.mp3");
 		          tom3.play();
 		          break;
-		case "a": var tom4 = new Audio("sounds/4.mp3");
+		case "j": var tom4 = new Audio("sounds/4.mp3");
 		          tom4.play();
 		          break; 
-		case "s": var snare = new Audio("sounds/5.mp3");
+		case "k": var snare = new Audio("sounds/5.mp3");
 		          snare.play();
 		          break;
-		case "d": var crash = new Audio("sounds/6.mp3");
+		case "l": var crash = new Audio("sounds/6.mp3");
 		          crash.play();
 		          break;          
-		case "z": var kick = new Audio("sounds/7.mp3");
+		case "e": var kick = new Audio("sounds/7.mp3");
 		          kick.play();
 		          break;    
                   
-                  case "x": var crash = new Audio("sounds/8.mp3");
+                  case "u": var crash = new Audio("sounds/8.mp3");
 		          crash.play();
 		          break;       
-                  case "c": var crash = new Audio("sounds/9.mp3");
+                  case "t": var crash = new Audio("sounds/9.mp3");
 		          crash.play();
 		          break;       
 	}
